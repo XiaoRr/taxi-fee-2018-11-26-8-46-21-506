ba368c3b69cadbac8a7284b8f0f628886023f657
小R <xufujian777@gmail.com>
2019-02-26 20:00:56 +0800

    起步价测试用例+用例实现成功

--- main/main.js
+++ main/main.js
@@ -1,4 +1,5 @@
-module.exports = function main(distance) {
+//distance:行驶距离 pauseTime:停车时间
+module.exports = function main(distance,pauseTime) {
     //console.log("Debug Info");
     return 6;
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -2,18 +2,13 @@ const main = require('../main/main');
 
 describe('taxi fee', function () {
     // write your tests here...
-    it("Test",function(){
-        let result = main(1);
+    it("起步价测试",function(){
+        let result = main(0.1,0);
         expect(result).toBe(6);
     })
 
-    it("Test2",function(){
-        let result = main(1);
-        expect(result).toBe(6);
-    })
-
-    it("Test3",function(){
-        let result = main(1);
+    it("起步价测试2",function(){
+        let result = main(2,0);
         expect(result).toBe(6);
     })
 });
